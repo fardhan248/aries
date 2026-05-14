6a20a9be6b8ea3f7769a0fc528c9481d96de50d7
fardhan248 <findrayesa@gmail.com>
2026-05-14 18:31:59 +0700

    chore: fix some bugs in docker-compose and edit echo in bash files

--- model_files/run_ollama.sh
+++ model_files/run_ollama.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "Starting Ollama server..."
+echo "Starting Ollama LLM server..."
 ollama serve &
 
-echo "Ollama is ready, creating the model..."
+echo "Ollama is ready, creating the LLM model..."
 
 ollama create qwen_llm -f model_files/Modelfile
 ollama run qwen_llm
--- model_files/run_ollama_embedding.sh
+++ model_files/run_ollama_embedding.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "Starting Ollama server..."
+echo "Starting Ollama embedding server..."
 ollama serve &
 
-echo "Ollama is ready, creating the model..."
+echo "Ollama is ready, creating the embedding model..."
 
 ollama create qwen_embedding -f model_files/Modelfile_embedding
 ollama run qwen_embedding
